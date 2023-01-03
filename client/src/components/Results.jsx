fa2f48fb59c5d4d2233b9ff5225eee8897836c97
Darrell Thomas <darrellthomas@Darrells-MacBook-Pro.local>
2023-01-02 19:06:46 -0500

    wediditjoe

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,6 +1,7 @@
 import QuestionnaireForm from "./QuestionnaireForm";
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React,{useState, useEffect} from "react";
+
+import { json, useLocation } from "react-router-dom";
 import Home from "../assets/pexels-binyamin-mellish-106399.jpg";
 import { Link } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/client";
@@ -10,45 +11,58 @@ function Results() {
   const location = useLocation();
   const formData = location.state.formData;
 
-  //Pull API DATA
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '72ddbc841bmsh8b56aa2a6ef50e7p1e74f1jsn17c984253b3e',
-      'X-RapidAPI-Host': 'us-real-estate.p.rapidapi.com'
-    }
-  };
+  //Hook used to format data
   
-   fetch(`https://us-real-estate.p.rapidapi.com/for-sale?offset=0&limit=42&state_code=GA&city=Atlanta&sort=newest&beds_min=${formData.bedroomsandBath}&baths_min=2&property_type=multi_family&home_size_min=1200&stories=single`, options)
-    .then(response => response.json())
-    .then(response => somethingdata(response))
-    .catch(err => console.error(err));
+  const [data2, setData] = useState(null);
+
+  //Pull API DATA
+    useEffect(() => {
+      async function fetchData() {
+        const response = await fetch(`https://us-real-estate.p.rapidapi.com/for-sale?offset=0&limit=42&state_code=GA&city=Atlanta&sort=newest&beds_min=3&baths_min=2&property_type=multi_family&home_size_min=1200&stories=single`, {
+          method: 'GET',
+          headers: {
+            'X-RapidAPI-Key': '72ddbc841bmsh8b56aa2a6ef50e7p1e74f1jsn17c984253b3e',
+            'X-RapidAPI-Host': 'us-real-estate.p.rapidapi.com',
+          },
+        });
+        const json = await response.json();
+        console.log(json,"RIGHT HERE")
+        setData(json);
+      }
+      fetchData();
+    }, []);
+
+  
 
 
-  // const { loading, data } = useQuery(QUERY_HOMES,{
-  //   variables:{area: formData.area,size: formData.space, bedroomsandBath: formData.bedroomsandBath, stories:formData.stories,style:formData.style}
-  // } );
-const somethingdata = (arg) =>{
-  console.log(arg.response,"DWEDWEFEWFWFEWF")
-}
-  // console.log(data,"idk")
 
 
- 
+
+
 
   console.log(formData,"What")
 
 
 
+
+//This break down the data to an object
+  const { status,data } = data2 || {};
+
+  console.log(data.results[1].primary_photo.href,"What??")
+ 
+
   return (
     <div className="results flex justify-center items-center">
       <div className="rounded-lg m-20 h-1/2 w-2/3">
         <div className="question-form  subpixel-antialiased text-center flex items-center font-sans text-lg sm:text-7xl sm: text-white">
-          <img className="homeimage" alt="homeimage" src={Home}></img>
+          <img className="homeimage" alt="homeimage" src={data.results[1].primary_photo.href}></img>
         </div>
         <div className="grid grid-col-1">
           <div className=" font-extrabold  grid flex justify-center">
+            <h2 className="text-xl">Price:</h2>
+            <p className="bg-white text-amber-300 text-2xl">{data.results[1].list_price}</p>
+            <br/>
+            <br/>
             <h1>Your Criteria:</h1>
             <p>
               <br />
@@ -67,8 +81,11 @@ const somethingdata = (arg) =>{
             <n />
             <p>Style : {formData.style}</p>
             <n />
-            <p>Floors: {formData.stories}</p>
+            <p>Floors: {formData.stories} 
+            </p>
             <n />
+            
+      
           </div>
           <div className="flex flex-col justify-center sm:flex-row ">
             <button className="m-10 w-1/2 bg-[#1497D4] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
